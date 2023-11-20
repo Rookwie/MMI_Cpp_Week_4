e4a5f7ea1eadab00448cc120e541f93d6db3e893
Rookwie <109480642+Rookwie@users.noreply.github.com>
2023-11-20 11:26:00 +0100

    w/e

---
+++ cmake-build-debug/CMakeFiles/MMI_Cpp_Week_4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MMI_Cpp_Week_4.dir/main.cpp.obj"
+  "CMakeFiles/MMI_Cpp_Week_4.dir/main.cpp.obj.d"
+  "MMI_Cpp_Week_4.exe"
+  "MMI_Cpp_Week_4.exe.manifest"
+  "MMI_Cpp_Week_4.pdb"
+  "libMMI_Cpp_Week_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MMI_Cpp_Week_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
